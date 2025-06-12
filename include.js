aa6631b9a9511a567334b8090695ef47b6e95115
mohammad <mohammadsilawy73@gmail.com>
2025-06-13 02:23:46 +0300

    new hirreciy

--- include.js
+++ include.js
@@ -3,8 +3,13 @@ document.addEventListener("DOMContentLoaded",  ()=> {
   includes.forEach(
     async function(el) {
     const file = el.getAttribute("data-include");
-    const response = await fetch("./"+file);
-    if (response.ok) {
+   const depth = window.location.pathname.split("/").length - 2;
+    
+    // Go back folders (e.g., '../', '../../', etc.)
+    const prefix = '../'.repeat(depth);
+
+    // Fetch file from root relative to current depth
+    const response = await fetch(prefix + file);    if (response.ok) {
       el.innerHTML = await response.text();
     } else {
       el.innerHTML = "<p>Error loading " + file + "</p>";
