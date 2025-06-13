fd8516218e7c295a2bd9a9fdf210a8170b0e599e
mohammad <mohammadsilawy73@gmail.com>
2025-06-14 00:39:08 +0300

    new

---
+++ auth/login.js
@@ -0,0 +1,9 @@
+document.getElementById("form").addEventListener("submit",(e)=>{
+    e.preventDefault();
+    
+    let username=document.getElementById("username");
+    let password=document.getElementById("password");
+
+    
+
+})
