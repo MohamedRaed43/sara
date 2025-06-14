b80da1802a4c243434fd7b4dd9bc3799dafec9bb
mohammad <mohammadsilawy73@gmail.com>
2025-06-14 21:48:54 +0300

    new

--- auth/login.js
+++ auth/login.js
@@ -7,11 +7,11 @@ document.getElementById("form").addEventListener("submit",(e)=>{
       db.collection("auth").get().then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
        if(doc.data().username==username.value && doc.data().password ==password.value){
-          ((name,logged_in,value2,value,days)=>{
+          ((name,value,days)=>{
             const expire=new Date();
               expire.setTime(expire.getTime() + (days*24*60*60*1000))
-              document.cookie=`${name}=${value};${logged_in}=${value2};  expires=${expire.toUTCString()}; path=/`;
-          })("username",true,username.value,1);
+              document.cookie=`${name}=${value}; expires=${expire.toUTCString()}; path=/`;
+          })("username",username.value,1);
        }
        else{
           let error=document.getElementById("error");
