0c8b8b90623465ca232cc3de8c07802812c0515a
mohammad <mohammadsilawy73@gmail.com>
2025-06-14 20:34:25 +0300

    new

--- auth/login.js
+++ auth/login.js
@@ -1,14 +1,24 @@
-  import db from '../index.js';
-
-document.addEventListener("DOMContentLoaded",()=>{
-  document.getElementById("form").addEventListener("submit",(e)=>{
+document.getElementById("form").addEventListener("submit",(e)=>{
     e.preventDefault();
     
     let username=document.getElementById("username");
     let password=document.getElementById("password");
-
-
-})
-
+      db.collection("auth").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+       if(doc.username==username.value && doc.password ==password){
+          ((name,vlaue,days)=>{
+            const expire=new Date();
+              expire.setTime(expire.getTime() + (days*24*60*60*1000))
+              document.cookie=`${name}=${value}; expires=${expire.toUTCString()}; path=/`;
+          })("username",username.value,1);
+       }
+       else{
+          let error=document.getElementsByClassName(".error")[0];
+          error.classList.toggle("error-vis");
+          username.classList.toggle("error-inputs");
+          password.classList.toggle("error-inputs")
+       }
+      });
+    }).catch(console.error);
 
 })
